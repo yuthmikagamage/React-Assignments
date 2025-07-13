9c5ea84acacdffe166c5dcc66823b494fae6fdba
yuthmikagamage <vanujyuthmika@gmail.com>
2025-07-13 20:33:22 +0530

    Final Assignment 10

--- src/assignments/Assignment_10.jsx
+++ src/assignments/Assignment_10.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react"
 import "./Assignment_10.css"
+import axios from "axios"
 function Assignment_10(){
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [displayResponce, setDisplayResponce] = useState("")
+
+    useEffect(()=>{
+        setDisplayResponce("")
+    },[email,password])
+
+    function submit(){
+        console.log("Button Clicked Submitted")
+        axios.post("https://auth.dnjs.lk/api/login",{
+            "email":email,
+            "password":password
+        }).then(responce=>{
+            console.log(responce)
+            setDisplayResponce("Correct Credentials")
+        }).catch(setDisplayResponce("Invalid Credentials"))
+    }
 
     return(
         <div>
             <h1>Assignment 10</h1>
+            <div className="userInput">
+                <form onSubmit={submit}>
+                    <label>Enter your email: </label>
+                    <input type="text" value={email} onChange={event=>setEmail(event.target.value)}></input>
+                    <label>Enter your password: </label>
+                    <input type="password" value={password} onChange={event=>setPassword(event.target.value)}></input>
+                    <button type="submit" className="fbutton">Submit</button>
+                </form>
+            </div>
+            <h3>{displayResponce}</h3>
         </div>
     )
-
 }
 export default Assignment_10
