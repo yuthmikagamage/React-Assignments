c5ad9915428d993ae28e2f94b465556e751e5e01
yuthmikagamage <vanujyuthmika@gmail.com>
2025-07-15 21:30:20 +0530

    Completed Assignment 13

--- src/assignments/Assignment_13.jsx
+++ src/assignments/Assignment_13.jsx
@@ -1,42 +1,104 @@
 import { useEffect, useState } from "react"
 import "./Assignment_13.css"
+import axios from "axios"
 
-function ProfileScreen({setLogin}){
+function ProfileScreen({token, setToken}){
+    const [name,setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [avatar, setAvatar] = useState()
 
+    axios.get("https://auth.dnjs.lk/api/user",{
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    }).then(responce=>{
+        const data = responce.data
+        setName(data.name)
+        setAvatar(data.avatar)
+        setDescription(data.description)
+    }).catch(error=>{
+        console.log(error.responce.data.error.message)
+    })
+
+    function logOut(){
+        axios.post("https://auth.dnjs.lk/api/logout",{}, {
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        }).then(responce=>{
+            console.log(responce)
+            sessionStorage.removeItem("token") 
+            localStorage.removeItem("token")
+            setToken("")
+        })
+    }
     return(
         <div>
-            <h1>Profile Screen</h1>
-            <button onClick={() => setLogin(false)}>Click me</button>
-        </div>
-    )}
-
-function LogingScreen({ setLogin }){
-
-    return(
-        <div>
-            <h1>Login Screen</h1>
-            <button onClick={() => setLogin(true)}>Click me</button>
+            <div className="displayUserData">
+                <img src={avatar} className="avatar"></img>
+                <h3>{name}</h3>
+                <h3>{description}</h3>
+                {name && <button className="fbutton" onClick={logOut}>Log Out</button>}
+            </div>
         </div>
     )
 }
 
+function LogingScreen({ setToken }){
+    const [check,setCheck] = useState(false)
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [error,setError] = useState("")
 
+    useEffect(()=>{
+        setError("")
+    },[email,password])
 
+    function submit(){
+        axios.post("https://auth.dnjs.lk/api/login",{
+        "email":email,
+        "password":password
+        }).then(responce=>{
+            console.log(responce)
+            setToken(responce.data.access_token)
+            if(check){
+                localStorage.setItem("token",responce.data.access_token)
+            }else if(!check){
+                sessionStorage.setItem("token",responce.data.access_token)
+            }
+        }).catch(error=>{
+            console.log(error.response?.data?.error?.message)
+            setError(error.response?.data?.error?.message)
+        })
+    }
 
+    return(
+        <div className="userInput">
+            <div className="inputBox">
+            <input type="text" placeholder="Your Email" value={email} onChange={event=>setEmail(event.target.value)}></input>
+            <input type="password" placeholder="Your Password" value={password} onChange={event=>setPassword(event.target.value)}></input>
+            </div>
+            <label>Keep me sign in</label>
+            <input type="checkbox" onChange={event=>setCheck(event.target.checked)}></input>
+            <button className="fbutton" onClick={submit}>Log In</button>
+            <h3>{error}</h3>
+        </div>
+    )
+}
 
 function Assignement_13(){
-    const [login, setLogin] = useState(false)
     const [token, setToken] = useState("")
 
     useEffect(()=>{
-
+        const token = sessionStorage.getItem("token") || localStorage.getItem("token")
+        setToken(token)
     },[])
     
     return(
         <div>
             <h1>Assignment 13</h1>
-            {login && <ProfileScreen setLogin={setLogin}  setToken={setToken}/>}
-            {!login && <LogingScreen setLogin={setLogin} token={token}/>}
+            {token && <ProfileScreen token={token} setToken={setToken}/>}
+            {!token && <LogingScreen setToken={setToken}/>}
         </div>
     )
 }
