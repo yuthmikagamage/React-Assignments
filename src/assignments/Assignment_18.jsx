19dfb7142ff272d4505d1d65948cf4fb7dca1d7a
yuthmikagamage <vanujyuthmika@gmail.com>
2025-07-18 16:28:57 +0530

    Assignment 18 Structure

--- src/assignments/Assignment_18.jsx
+++ src/assignments/Assignment_18.jsx
@@ -1,9 +1,41 @@
-
+import { useEffect, useState } from "react"
 import "./Assignment_18.css"
 
 function Assignment_18(){
+    const [colorArray, setColorArray] = useState([])
+    const [randomNo, setrandomNo] = useState(0)
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            colourAdding()
+        },1000)
+
+        return ()=>clearInterval(interval)
+    },[])
+
+    function colourAdding(){
+        
+        const random = Math.random()
+        let color = ""
+        setrandomNo(random)
+        if(random<0.5){
+            color = "blue"
+        }else{
+            color="red"
+        }
+        setColorArray(prevArray =>[color,...prevArray])
+    }
+
     return(
-        <h1>Assignment 18</h1>
+        <div className="div">
+            <h1>Assignment 18</h1>
+            <h3>{randomNo}</h3>
+            <ul>
+                {colorArray.map((color,key)=>(
+                    <li key={key}>{color}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
 export default Assignment_18
