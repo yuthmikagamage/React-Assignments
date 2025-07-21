c87d75bcb84de17109e5f319e271cf207d0381c2
yuthmikagamage <vanujyuthmika@gmail.com>
2025-07-21 23:14:49 +0530

    Completed Assignment 19

--- src/assignments/Assignment_19.jsx
+++ src/assignments/Assignment_19.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react"
 function Assignment_19(){
     const [questions,setQuestions] = useState([])
     const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [score, setScore] = useState(0);
+    const [endGame, setEndGame] = useState(false)
 
     function loadQuestions(){
         axios.get("https://apis.dnjs.lk/objects/quiz.php").then(
@@ -21,15 +23,44 @@ function Assignment_19(){
         loadQuestions()
     },[])
 
+    function handleUserChoise(index){
+        console.log("Correct index is "+ questions[currentQuestion].correct)
+        console.log(`Selected index is ${index}`)
+        if(index === questions[currentQuestion].correct){
+            setScore(score+1)
+        }
+        if(currentQuestion<9){
+            setCurrentQuestion(currentQuestion+1)
+        }else{
+            setEndGame(true)
+        }
+    }
+
 
     return(
         <div>
             <h1>Assignment 19</h1>
-            {questions.length > 0 ? (
-                <h2>{questions[currentQuestion].question}</h2>
-            ) : (
-                <h2>Loading question...</h2>
-            )}
+            <div className="assignment-19">
+                {!endGame && <div className="questons">
+                    {questions.length > 0 ? (
+                        <div>
+                            <h2>{currentQuestion+1}. {questions[currentQuestion].question}</h2>
+                            <ul>
+                                {questions[currentQuestion].answers.map((answer,index)=>(
+                                    <li key={index} onClick={()=>handleUserChoise(index)}>{index+1}. {answer}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ) : (
+                        <h2>Loading questions...</h2>
+                    )}
+                </div>}
+                {endGame && 
+                <div className="endScreen">
+                    <h2>Game Over!</h2>
+                    <h2>Your Score is {score}</h2>
+                </div>}
+            </div>
         </div>
     )
 
