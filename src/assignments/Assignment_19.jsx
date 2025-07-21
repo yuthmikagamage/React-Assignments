9cc0e768204a2fd115940b3b7bca439c4cb62185
yuthmikagamage <vanujyuthmika@gmail.com>
2025-07-21 09:48:42 +0530

    API called - 19

--- src/assignments/Assignment_19.jsx
+++ src/assignments/Assignment_19.jsx
@@ -1,8 +1,36 @@
-
+import axios from "axios"
+import "./Assignment_19.css"
+import { useEffect, useState } from "react"
 
 function Assignment_19(){
+    const [questions,setQuestions] = useState([])
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
+    function loadQuestions(){
+        axios.get("https://apis.dnjs.lk/objects/quiz.php").then(
+            responce=>{
+                console.log(responce.data)
+                setQuestions(responce.data)
+            }
+        ).catch(error=>{
+            console.log(error)
+        })
+    }
+
+    useEffect(()=>{
+        loadQuestions()
+    },[])
+
+
     return(
+        <div>
             <h1>Assignment 19</h1>
+            {questions.length > 0 ? (
+                <h2>{questions[currentQuestion].question}</h2>
+            ) : (
+                <h2>Loading question...</h2>
+            )}
+        </div>
     )
 
 }
