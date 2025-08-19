d93dd185ba4b3ff2b5855c98066a53a0a9e6c87a
yuthmikagamage <vanujyuthmika@gmail.com>
2025-08-19 10:15:38 +0530

    Completed Task 13

--- src/tasks/Task_13.jsx
+++ src/tasks/Task_13.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Task_13.css";
 
 function Task_13() {
   const [currentInput, setCurrentInput] = useState("");
   const [itemsOutput, setItemsOutput] = useState([]);
+  const currentCommandIndex = useRef(-1);
 
   function executeCommads(userInput) {
     try {
@@ -11,25 +12,62 @@ function Task_13() {
       const func = new Function("return (" + userInput + ")");
       const result = func();
       console.log(result);
-      setItemsOutput((prev) => [
-        ...prev,
-        { command: userInput, output: String(result) },
-      ]);
+
+      setItemsOutput((prev) => {
+        const newOutput = [
+          ...prev,
+          {
+            command: userInput,
+            output: String(result),
+            color: "cornflowerblue",
+          },
+        ];
+        currentCommandIndex.current = -1;
+        return newOutput;
+      });
     } catch (error) {
       console.log(error.message);
-      setItemsOutput((prev) => [
-        ...prev,
-        { command: userInput, output: error.message },
-      ]);
+
+      setItemsOutput((prev) => {
+        const newOutput = [
+          ...prev,
+          { command: userInput, output: error.message, color: "red" },
+        ];
+        currentCommandIndex.current = -1;
+        return newOutput;
+      });
     }
   }
 
   function pressKeyDown(event) {
-    if (event.key == "Enter") {
+    if (event.key === "Enter") {
       event.preventDefault();
       console.log("Pressed Enter");
       executeCommads(currentInput);
       setCurrentInput("");
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      if (itemsOutput.length === 0) return;
+      if (currentCommandIndex.current === -1) {
+        currentCommandIndex.current = itemsOutput.length - 1;
+      } else if (currentCommandIndex.current > 0) {
+        currentCommandIndex.current -= 1;
+      }
+
+      setCurrentInput(itemsOutput[currentCommandIndex.current].command);
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+
+      if (itemsOutput.length === 0 || currentCommandIndex.current === -1)
+        return;
+
+      if (currentCommandIndex.current < itemsOutput.length - 1) {
+        currentCommandIndex.current += 1;
+        setCurrentInput(itemsOutput[currentCommandIndex.current].command);
+      } else {
+        currentCommandIndex.current = -1;
+        setCurrentInput("");
+      }
     }
   }
 
@@ -40,7 +78,9 @@ function Task_13() {
           {itemsOutput.map((item, index) => (
             <div className="commandresult" key={index}>
               <div className="command">- {item.command}</div>
-              <div className="output">{item.output}</div>
+              <div className="output" style={{ color: item.color }}>
+                {item.output}
+              </div>
             </div>
           ))}
         </div>
@@ -50,7 +90,7 @@ function Task_13() {
           value={currentInput}
           onChange={(event) => setCurrentInput(event.target.value)}
           placeholder="Enter JavaScript Commands"
-        ></input>
+        />
       </div>
     </div>
   );
