e2b8d01c50a48d0ca590b0931f711b819fd9151b
yuthmikagamage <vanujyuthmika@gmail.com>
2026-01-20 11:09:56 +0530

    Water sort basic

--- src/tasks/Task_24.jsx
+++ src/tasks/Task_24.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 import "./Task_24.css";
 
+const COLORS = [
+  "rgb(51, 153, 255)",
+  "rgb(255, 51, 51)",
+  "rgb(0, 204, 102)",
+  "rgb(153, 51, 51)",
+  "rgb(153, 51, 255)",
+  "rgb(255, 153, 51)",
+];
+
+const BOTTLE_CAPACITY = 4;
+const INITIAL_FILL = 3;
+const TOTAL_BOTTLES = 8;
+
 function Task_24() {
-  const colors = [
-    "rgb(51, 153, 255)",
-    "rgb(255, 51, 51)",
-    "rgb(0, 204, 102)",
-    "rgb(153, 51, 51)",
-    "rgb(153, 51, 255)",
-    "rgb(255, 153, 51)",
-  ];
   const [bottles, setBottles] = useState([]);
+  const [selectedBottle, setSelectedBottle] = useState(null);
 
   useEffect(() => {
     initializeGame();
@@ -18,22 +24,70 @@ function Task_24() {
 
   function initializeGame() {
     let colorPool = [];
-    colors.forEach((color) => {
-      for (let i = 1; i <= 4; i++) {
+
+    COLORS.forEach((color) => {
+      for (let i = 0; i < BOTTLE_CAPACITY; i++) {
         colorPool.push(color);
       }
     });
 
     colorPool.sort(() => Math.random() - 0.5);
+
     const newBottles = [];
-    for (let i = 1; i <= 8; i++) {
-      newBottles.push(colorPool.splice(0, 3));
+    for (let i = 0; i < TOTAL_BOTTLES; i++) {
+      newBottles.push(colorPool.splice(0, INITIAL_FILL));
     }
+
+    setBottles(newBottles);
+    setSelectedBottle(null);
+  }
+
+  function getTopColor(bottle) {
+    return bottle.length === 0 ? null : bottle[bottle.length - 1];
+  }
+
+  function canTransfer(fromBottle, toBottle) {
+    if (fromBottle.length === 0) return false;
+    if (toBottle.length >= BOTTLE_CAPACITY) return false;
+    if (toBottle.length === 0) return true;
+    return getTopColor(fromBottle) === getTopColor(toBottle);
+  }
+
+  function transferLiquid(fromIndex, toIndex) {
+    const newBottles = bottles.map((b) => [...b]);
+    const fromBottle = newBottles[fromIndex];
+    const toBottle = newBottles[toIndex];
+
+    if (!canTransfer(fromBottle, toBottle)) return;
+
+    const movingColor = getTopColor(fromBottle);
+
+    while (
+      fromBottle.length &&
+      getTopColor(fromBottle) === movingColor &&
+      toBottle.length < BOTTLE_CAPACITY
+    ) {
+      toBottle.push(fromBottle.pop());
+    }
+
     setBottles(newBottles);
   }
 
   function clickBottle(bottleId) {
-    console.log("Clicked", bottleId);
+    if (selectedBottle === null) {
+      if (bottles[bottleId].length > 0) {
+        setSelectedBottle(bottleId);
+      }
+      return;
+    }
+
+    if (selectedBottle === bottleId) {
+      setSelectedBottle(null);
+      return;
+    }
+
+    transferLiquid(selectedBottle, bottleId);
+    setSelectedBottle(null);
   }
 
   return (
@@ -42,7 +96,9 @@ function Task_24() {
         {bottles.map((bottle, bottleId) => (
           <div
             key={bottleId}
-            className="bottle"
+            className={`bottle ${
+              selectedBottle === bottleId ? "selected" : ""
+            }`}
             onClick={() => clickBottle(bottleId)}
           >
             {bottle.map((color, colorID) => (
@@ -50,7 +106,7 @@ function Task_24() {
                 key={colorID}
                 className="liquid"
                 style={{ backgroundColor: color }}
-              ></div>
+              />
             ))}
           </div>
         ))}
@@ -58,4 +114,5 @@ function Task_24() {
     </div>
   );
 }
+
 export default Task_24;
