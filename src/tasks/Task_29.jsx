63865e4f759f52af02a068661e16471f5b900614
yuthmikagamage <vanujyuthmika@gmail.com>
2026-03-12 09:21:32 +0530

    Task 29 : Face detection II

--- src/tasks/Task_29.jsx
+++ src/tasks/Task_29.jsx
@@ -1,33 +1,62 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./Task_29.css";
 import * as faceapi from "face-api.js";
 
 const loadModels = async () => {
-  const MODEL_URL = "/models";
-
+  const MODEL_URL = "./models";
   await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
-
-  console.log(faceapi.nets);
-  detect();
-};
-
-const detect = async () => {
-  const img = await faceapi.fetchImage("/person.jpg");
-
-  const detections = await faceapi.detectAllFaces(img);
-
-  console.log("Detections:", detections);
+  console.log("Model Loaded");
 };
 
 function Task_29() {
+  const [image, setImage] = useState(null);
+  const imgRef = useRef();
+
   useEffect(() => {
     loadModels();
   }, []);
 
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
+  const detectFace = async () => {
+    if (!imgRef.current) return;
+
+    const detections = await faceapi.detectAllFaces(
+      imgRef.current,
+      new faceapi.SsdMobilenetv1Options(),
+    );
+
+    console.log("result:", detections);
+  };
+
   return (
     <div className="task-29">
-      <h2>Task 29</h2>
+      <div className="upload-box">
+        <input
+          type="file"
+          accept="image/*"
+          id="imageUpload"
+          onChange={handleImageUpload}
+        />
+
+        <label htmlFor="imageUpload" className="upload-btn">
+          Upload Image
+        </label>
+      </div>
+
+      {image && (
+        <div className="preview-container">
+          <img ref={imgRef} src={image} alt="preview" onLoad={detectFace} />
+        </div>
+      )}
     </div>
   );
 }
+
 export default Task_29;
